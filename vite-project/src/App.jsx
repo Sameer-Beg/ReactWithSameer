2acdb40f53e22818f63dfe9e167544872c75b40d
SameerBeg <sameer423j@gmail.com>
2025-07-27 18:40:57 +0530

    Files

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import S1_objDeclaration from './Components/S1_objDeclaration'
 import S2_ReusableComponents from './Components/S2_ReusableComponents'
 import S3_Props from './Components/S3_Props'
+import S4_MoreOnProps from './Components/S4_MoreOnProps'
 const App = () => {
   return (
     <>
@@ -14,10 +15,17 @@ const App = () => {
       <S2_ReusableComponents/>
     </div> */}
 
-    <div>
+    {/* <div>
       <S3_Props title="ApllePhone" brand="Aplle" price={20000}/>
       <S3_Props title="SonataWatch" brand="Sonata" price={3000}/>
     </div>
+
+    <div>
+      <S4_MoreOnProps title="Aplle" product="phone" price={30000} />
+      <S4_MoreOnProps title="Sonata" product="Watch" price={2000} />
+    </div> */}
+
+    <div className="sam">sameer is good developer  </div>
     </>
   )
 }
---
+++ vite-project/src/Components/S4_MoreOnProps.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const S4_MoreOnProps = ({title , product , price}) => {
+  return (
+    <div>
+      <h1>Title = {title}</h1>
+      <h1>Product = {product}</h1>
+      <h1>Price = {price}</h1>
+      
+    </div>
+  )
+}
+
+export default S4_MoreOnProps
